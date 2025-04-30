61158d8dac0c64f21a7e4add62f94724f86edb58
ianspractice <bodybody.ian@gmail.com>
2025-04-29 20:46:01 -0600

    Add variation of solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,21 +1,11 @@
 const palindromes = function (string) {
-  function filterArray(string) {
-    return /[a-z]|\d|\w/.test(string);
-  }
-  const cleanString = string
-    .toLowerCase()
+  const cleanStr = string
     .split("")
-    .filter(filterArray)
-    .join("");
-  const reverseString = cleanString.split("").reverse().join("");
-  console.log(`
-    clean: ${cleanString}
-    reverse: ${reverseString}`);
-  if (reverseString === cleanString) {
-    return true;
-  } else {
-    return false;
-  }
+    .filter((char) => /[^\W_]/.test(char))
+    .join("")
+    .toLowerCase();
+  const reverse = cleanStr.split("").reverse().join("");
+  return cleanStr === reverse;
 };
 
 // Do not edit below this line
